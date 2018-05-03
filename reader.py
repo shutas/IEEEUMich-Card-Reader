51c8d6def227bcef65f3ab723228334cad79282b
しゅーたす <shutas@umich.edu>
2018-05-02 21:13:42 -0400

    FIX: Style Errors

--- reader.py
+++ reader.py
@@ -6,26 +6,26 @@
 
 import smtplib
 import datetime
-from email.mime.text import MIMEText
+
 
 class CardProcessor(object):
     """Card Processor Class."""
+
     def __init__(self):
+        """Construct Card Processor Class."""
         self.event_name = ""
         self.sender_email = "ieee.umich.donotreply@gmail.com"
         self.sender_password = ""
         self.destination_email = "IEEE.VPComm@umich.edu"
         self.user_dict = {}
 
-
     def initialize(self):
         """Retrieve Email Credentials for Sending Email."""
-
         print("\n<=== Card Reader Started Execution ===>\n")
 
+        # Append Today's Date to Event Name
         self.event_name = input("Enter event name: ") + " " +\
-                          datetime.datetime.today().strftime('(%m/%d/%Y)')
-
+            datetime.datetime.today().strftime('(%m/%d/%Y)')
 
     def send_email(self):
         """Send the Result via Email."""
@@ -36,14 +36,15 @@ class CardProcessor(object):
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.ehlo()
         server.starttls()
-        
+
         # Authenticate User
         authenticated = False
 
         while not authenticated:
             # Get Password from User Input
-            self.sender_password = input("Enter password for ieee.umich.donotreply@gmail.com: ")
-        
+            self.sender_password = input("Enter password for " +
+                                         "ieee.umich.donotreply@gmail.com: ")
+
             # Check if Password is Correct
             try:
                 server.login(self.sender_email, self.sender_password)
@@ -54,7 +55,9 @@ class CardProcessor(object):
                 print("-> FAIL: Password incorrect\n")
 
         # Get Destination Email
-        destination_email = input("Enter email destination address (if not specified, it will send to VPComm): ")
+        destination_email = input("Enter email destination address " +
+                                  "(if not specified, " +
+                                  "it will send to VPComm): ")
 
         # Update Destination Email if Necessary
         if destination_email:
@@ -76,21 +79,22 @@ class CardProcessor(object):
 
         # Check if Email was Sent Successfully
         try:
-            server.sendmail(self.sender_email, [self.destination_email], message)
-            print ('-> SUCCESS: Email sent to ' + self.destination_email + "\n")
+            server.sendmail(self.sender_email,
+                            [self.destination_email],
+                            message)
+            print("-> SUCCESS: Email sent to " + self.destination_email + "\n")
         # If Sending Failed for Some Reason
-        except:
-            print ('-> FAIL: Email was NOT sent\n')
+        except smtplib.SMTPException:
+            print("-> FAIL: Email was NOT sent\n")
 
         # Terminate SMTP Server
         server.quit()
-
 
     def process_mcard(self, input_data):
         """Parse MCard Data and Add to Spreadsheet."""
         # Sanitize Input Data
         uniqname = input_data.lower()
-        
+
         # Update User Dictionary
         if uniqname in self.user_dict:
             self.user_dict[uniqname] += 1
