683fa678458605feabd6bb435896abad7e0678cb
しゅーたす <shutas@umich.edu>
2018-05-08 14:42:54 -0400

    MOD: Added Packages for Google API

--- setup.py
+++ setup.py
@@ -6,9 +6,13 @@ setup(
     packages=['ieee_signin'],
     include_package_data=True,
     install_requires=[
+        "gspread==3.0.0",
+        "google-api-python-client==1.6.7",
+        "oauth2client==4.1.2",
         "pycodestyle==2.4.0",
         "pydocstyle==2.1.1",
-        "pylint==1.8.4"
+        "pylint==1.8.4",
+        "PyOpenSSL==17.5.0"
     ],
     entry_points={
         'console_scripts': [
